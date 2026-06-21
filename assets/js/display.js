d845cd9835380a40c09cbb6737d680b3067043c7
Su-1972 <99266495+RectHua@users.noreply.github.com>
2026-06-21 12:51:37 +0800

    Add files via upload
    Uploaded a new version of the website. The display section in the main page was redesigned for better experience, and we added some pictures and new locomotives for display.

--- assets/js/display.js
+++ assets/js/display.js
@@ -8,19 +8,23 @@ const imgUrls = [
   './assets/img/display/3.png',
   './assets/img/display/4.png',
   './assets/img/display/5.png',
-  './assets/img/display/6.png'
+  './assets/img/display/6.png',
+  './assets/img/display/7.png',
+  './assets/img/display/8.png'
 ];
 
 const imgTexts = [
-  'HXN5 型机车',
+  'HXN5B 型机车',
   'DF5 型机车',
   'HXD1 型机车',
   '百年滇越行',
   '毛号HXD3D',
-  'YZ22型客车'
+  '小站一角',
+  'HXD1擦肩而过',
+  '高光下的GK1E'
 ];
 
-let currentIndex = 0;
+let currentIndex = 6;
 
 function updateDisplay() {
   dspPic.style.backgroundImage = 'url('+imgUrls[currentIndex]+')';
